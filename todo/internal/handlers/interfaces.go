58a8a8ee337678b7e86fbf66966b8e846c50f109
IKostarev <ilya.kostarev.9@gmail.com>
2023-11-23 09:50:47 +0500

    upd interfaces

--- todo/internal/handlers/interfaces.go
+++ todo/internal/handlers/interfaces.go
@@ -9,7 +9,7 @@ import (
 type TodoService interface {
 	CreateToDo(ctx context.Context, newTodo *models.TodoDTO) (*models.TodoDTO, error)
 	UpdateToDo(ctx context.Context, newTodo *models.TodoDTO) (*models.TodoDTO, error)
-	GetToDos(ctx context.Context, todos *models.GetTodosDTO) ([]models.TodoDTO, error)
+	GetToDos(ctx context.Context) ([]models.TodoDTO, error)
 	GetToDo(ctx context.Context, todoID uuid.UUID) (*models.TodoDTO, error)
 	DeleteToDo(ctx context.Context, todoID uuid.UUID) error
 }

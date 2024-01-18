72e4e41f0ce043bcee32b7cff9aba2cbfb824d0a
Egor Popov <ep.egor.popov@gmail.com>
2024-01-18 20:30:50 +0700

    added swagger docs

--- gateway/internal/api/rest/server.go
+++ gateway/internal/api/rest/server.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 	"gateway/config"
 	rest "gateway/internal/api"
+	_ "gateway/internal/api/docs"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"net/http"
 )
 
+// @title           ToDo Gateway API
+// @version         1.0
+// @description     This service is Gateway API for all microservices of ToDo service
+// @host            localhost:3000
+// @BasePath        /api
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 func RunREST(
 	cfg *config.Config,
 	logger *zerolog.Logger,
@@ -21,15 +32,18 @@ func RunREST(
 		ValidateTokenMiddleware(
 			&cfg.JWT,
 			[]string{
+				"/docs/swagger",
 				"/api/v1/users/login",
 				"/api/v1/users/register",
 			},
 		),
 	)
 
+	router.PathPrefix("/docs/swagger").Handler(httpSwagger.WrapHandler)
+
 	usersV1Router := router.PathPrefix("/api/v1/users").Subrouter()
 	usersV1Router.HandleFunc("/register", gatewayHandler.RegisterUser).Methods(http.MethodPost)
-	usersV1Router.HandleFunc("/{id:[0-9]+}", gatewayHandler.GetGetUserById).Methods(http.MethodGet)
+	usersV1Router.HandleFunc("/{id:[0-9]+}", gatewayHandler.GetUserById).Methods(http.MethodGet)
 	usersV1Router.HandleFunc("/update", gatewayHandler.UpdateUser).Methods(http.MethodPut)
 	usersV1Router.HandleFunc("/update-password", gatewayHandler.UpdatePassword).Methods(http.MethodPut)
 	usersV1Router.HandleFunc("/delete/{id:[0-9]+}", gatewayHandler.DeleteUser).Methods(http.MethodDelete)
